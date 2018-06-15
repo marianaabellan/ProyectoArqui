25343945c78c9303bccf56c51285eec42eeb756d
Ricardo Corrales Barquero <ricorrales07@gmail.com>
2018-06-15 13:28:01 -0600

    Imprimiendo datos de hilillos al final.

--- SimuladorMIPS/ConsoleApp1/Hilillo.cs
+++ SimuladorMIPS/ConsoleApp1/Hilillo.cs
@@ -13,15 +13,30 @@ namespace SimuladorMIPS
         {
             PC = direccionDeInicio;
             this.Nombre = nombre;
+            Registro = new int[32];
+            Ciclos = 0;
         }
 
-        // TODO: Implementar función.
+        // Esta función se llama al final para imprimir datos de un hilillo finalizado.
         public string PrettyPrintRegistrosYCiclos()
         {
-            throw new NotImplementedException();
+            string output = "";
+
+            output += Nombre + ":\n"
+                + "Ciclos: " + Ciclos + "\n"
+                + "Registros: ";
+
+            for (int i = 0; i < 32; i++)
+            {
+                output += Registro[i] + " ";
+            }
+
+            return output;
         }
 
         public int PC { get; set; }
         public string Nombre { get; }
+        public int[] Registro { get; set; }
+        public int Ciclos;
     }
 }
